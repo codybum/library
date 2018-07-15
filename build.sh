ab1268cb075a17d9ffd2a07cca9685b7d32073b4
Cody Bumgardner <cody@uky.edu>
2018-07-15 14:15:04 -0400

    k

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 mvn clean package bundle:bundle 
-cp target/library-1.0-SNAPSHOT.jar /Users/cody/IdeaProjects/agent/jars 
-cp target/library-1.0-SNAPSHOT.jar /Users/cody/IdeaProjects/felix-framework-5.6.10/bundle
-cp pom.xml /Users/cody/IdeaProjects/repo/io/cresco/library/1.0-SNAPSHOT/library-1.0-SNAPSHOT.pom
-cp target/library-1.0-SNAPSHOT.jar /Users/cody/IdeaProjects/repo/io/cresco/library/1.0-SNAPSHOT
+cp pom.xml ../repo/io/cresco/library/1.0-SNAPSHOT/library-1.0-SNAPSHOT.pom
+cp target/library-1.0-SNAPSHOT.jar ../repo/io/cresco/library/1.0-SNAPSHOT
+cp target/library-1.0-SNAPSHOT.jar ../agent/src/main/resources
+
+
+#mvn clean package bundle:bundle 
+#cp target/library-1.0-SNAPSHOT.jar /Users/cody/IdeaProjects/agent/jars 
+#cp target/library-1.0-SNAPSHOT.jar /Users/cody/IdeaProjects/felix-framework-5.6.10/bundle
+#cp pom.xml /Users/cody/IdeaProjects/repo/io/cresco/library/1.0-SNAPSHOT/library-1.0-SNAPSHOT.pom
+#cp target/library-1.0-SNAPSHOT.jar /Users/cody/IdeaProjects/repo/io/cresco/library/1.0-SNAPSHOT
